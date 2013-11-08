3d08de1414e1c6635639da14f4fe1c201774d617
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-08 17:20:07 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/recursion "0.1.0-SNAPSHOT"
+  :description "tests for recursion"
+  :url "http://github.com/iloveponies/recusion"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])

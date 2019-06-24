5b39edeb03a36c174e015a520c00b5fe1a6e7bc7
Ilmo Raunio <ilmo.raunio@gmail.com>
2019-06-24 18:35:14 +0300

    Add support for latest jdk (11)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject iloveponies.tests/recursion "0.1.0-SNAPSHOT"
+(defproject iloveponies.tests/recursion "0.2.0-SNAPSHOT"
   :description "tests for recursion"
   :url "http://github.com/iloveponies/recusion"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [midje "1.9.4"]
                  [midje-grader "0.1.0-SNAPSHOT"]])
